8af422d153e18aa4f96eb94b3263b080bb1fabf6
Mauricio Aspee <ale_aspee@hotmail.com>
2022-07-05 20:33:12 -0400

    Versión 2
    Actualizacion de settings para estilos.css

--- monitor/monitor/settings.py
+++ monitor/monitor/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'monitor/static'),)
 LOGIN_REDIRECT_URL = 'inicio'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
